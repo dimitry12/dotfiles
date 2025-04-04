5a7e0786f0b8364cf366c0bbc14e7b6ed47e4ceb
Dzmitry Pletnikau <dzmitry@pletnikau.com>
2025-04-04 08:37:10 -0600

    added custom command

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -3,3 +3,9 @@
 -- Add any additional options here
 
 vim.g.ai_cmp = false
+
+vim.api.nvim_create_user_command("Cppath", function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  vim.notify('Copied "' .. path .. '" to the clipboard!')
+end, {})
