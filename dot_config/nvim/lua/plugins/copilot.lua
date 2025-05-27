5a0b09b1ab414099cd4e215d1728401e67ca0da8
Dzmitry Pletnikau <dzmitry@pletnikau.com>
2025-05-27 12:39:26 -0600

    make copilot log for endpoint debugging

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -1,16 +1,27 @@
 return {
-  "zbirenbaum/copilot.lua",
-  keys = {
-    {
-      "<leader>at",
-      function()
-        if require("copilot.client").is_disabled() then
-          require("copilot.command").enable()
-        else
-          require("copilot.command").disable()
-        end
-      end,
-      desc = "Toggle (Copilot)",
-    },
-  },
+	"zbirenbaum/copilot.lua",
+	keys = {
+		{
+			"<leader>at",
+			function()
+				if require("copilot.client").is_disabled() then
+					require("copilot.command").enable()
+				else
+					require("copilot.command").disable()
+				end
+			end,
+			desc = "Toggle (Copilot)",
+		},
+	},
+	opts = {
+		logger = {
+			log_to_file = false,
+			file = vim.fn.stdpath("log") .. "/copilot-lua.log",
+			file_log_level = vim.log.levels.WARN,
+			print_log = true,
+			print_log_level = vim.log.levels.WARN,
+			trace_lsp = "off", -- "off" | "messages" | "verbose"
+			trace_lsp_progress = false,
+		},
+	},
 }
