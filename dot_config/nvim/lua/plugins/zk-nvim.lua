f8fc9c1ad7578146a39e26d081b8c71b437d3022
Dzmitry Pletnikau <dzmitry@pletnikau.com>
2026-03-27 16:21:10 -0600

    zk shortcuts

--- dot_config/nvim/lua/plugins/zk-nvim.lua
+++ dot_config/nvim/lua/plugins/zk-nvim.lua
@@ -1,8 +1,15 @@
 return {
   "zk-org/zk-nvim",
   config = function()
-    require("zk").setup({
-      -- See Setup section below
-    })
+    require("zk").setup()
+
+    local opts = { noremap = true, silent = false }
+
+    -- Global mappings
+    vim.api.nvim_set_keymap("n", "<leader>zn", "<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>", opts)
+    vim.api.nvim_set_keymap("n", "<leader>zo", "<Cmd>ZkNotes { sort = { 'modified' } }<CR>", opts)
+    vim.api.nvim_set_keymap("n", "<leader>zt", "<Cmd>ZkTags<CR>", opts)
+    vim.api.nvim_set_keymap("n", "<leader>zf", "<Cmd>ZkNotes { sort = { 'modified' }, match = { vim.fn.input('Search: ') } }<CR>", opts)
+    vim.api.nvim_set_keymap("v", "<leader>zf", ":'<,'>ZkMatch<CR>", opts)
   end,
 }
